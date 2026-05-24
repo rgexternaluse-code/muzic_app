b53d5a171a20bf8eec09c7deb06fc153ec43fd77
RG Singh <rg.externaluse@gmail.com>
2026-05-24 17:21:07 +0530

    capacitor fix

---
+++ android/app/src/main/java/com/muzic/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.muzic.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.muzic.app',
+  appName: 'Muzic',
+  webDir: 'dist'
+};
+
+export default config;
